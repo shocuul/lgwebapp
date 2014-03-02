d8ff05f4a192e2b3ffe207250e5be7d056ce9b65
Jose David <josepdvahn@me.com>
2014-03-02 00:55:24 -0600

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Retusn the full title on a per-agebased
+  def full_title(page_title)
+    base_title = "LateGame"
+    if page_title.empty?
+      "#{base_title} | Latino America en llamas"
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
